25696a561c1131a39eb371b455bcf6f6bd9e7359
Leonela Nava <leonelaenava@gmail.com>
2021-12-10 22:01:15 -0500

    añadi interacciones con dom

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ class Productos {
 const ProductosSkincare = [];
 
 ProductosSkincare.push(new Productos (1, "Limpiador en Gel", 50, "limpiador"))
-ProductosSkincare.push(new Productos (1, "Limpiador Espuma", 30, "limpiador"))
+ProductosSkincare.push(new Productos (2, "Limpiador Espuma", 30, "limpiador"))
 ProductosSkincare.push(new Productos (3, "leche limpiadora", 40, "limpiador"))
 ProductosSkincare.push(new Productos (4,"serum vitamina C", 70, "serum"))
 ProductosSkincare.push(new Productos (5,"serum hidratante", 80, "serum"))
@@ -20,14 +20,16 @@ ProductosSkincare.push(new Productos (6,"bloqueador gel", 60, "bloqueador"))
 ProductosSkincare.push(new Productos (7,"bloqueador crema", 50, "bloqueador"))
 
 
+/* Creando elementos para el DOM desde JS */
 
-/* Funcion que muestra los productos disponibles */
-function MostrarProductos () {
-alert("Chequea los productos disponibles en el console log")
-console.log(ProductosSkincare);
+let ListaProductos = document.getElementById("ProductosSkincare")
+
+for (const producto of ProductosSkincare) {
+    let li = document.createElement("li");
+   li.innerHTML = JSON.stringify(producto);
+   ListaProductos.appendChild(li);
 }
 
-MostrarProductos ()
 
 /* Filtro para encontrar productos menores a 50*/
 
@@ -41,7 +43,14 @@ console.log(Menora50)
 IDingresada = parseInt (prompt("Ingrese ID del producto"));
 
 let IDencontrada = ProductosSkincare.find( producto => producto.id == IDingresada)
-console.log(IDencontrada)
+
+/* Mostrar ID encontrado en el HTML */
+
+let respuestaID = document.createElement("div");
+respuestaID.innerHTML = "El producto encontrado es " + JSON.stringify(IDencontrada);
+document.body.appendChild(respuestaID);
+
+
 
 
 
