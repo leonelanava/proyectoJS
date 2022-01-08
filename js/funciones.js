728a3ef691fd8503ea3cb17c7df87e99422ca770
Leonela Nava <leonelaenava@gmail.com>
2022-01-07 22:27:38 -0400

    guardando cambios

--- js/funciones.js
+++ js/funciones.js
@@ -1,9 +1,22 @@
+
+/* Añadiendo select de tipo de Productos */
+
+
+function filtroTipos(lista) {
+  for (const tipo of lista) {
+    $('filtroProductos').append('<option> ${tipo} </option>')
+  }
+}
+
+
+
 /* Función para generar productos en HTML */
 
 function productosHTML(Productos, id){
     $(id).empty();
     for (const producto of Productos) {
-       $(id).append(`<div class="card m-3" style="width: 18rem;">
+       $(id).append(`<div class="col-lg-4 col-md-4 col-sm-12">
+       <div class="card m-3" style="width: 18rem;">
                       <img src= "${producto.img}" class="card-img-top" alt="...">
                       <div class="card-body">
                         <h5 class="card-title">${producto.producto}</h5>
@@ -11,9 +24,11 @@ function productosHTML(Productos, id){
                         <p class="card-text">Categoría: ${producto.tipo}</p>                    
                         <a href="#" id='${producto.id}' class="btn btn-primary btn-agregar">Agregar a carrito</a>
                       </div>
+                    </div>
                     </div>`);
     }
   }
+
 
 /* Función para comprar producto */
 
@@ -27,33 +42,36 @@ function comprarProducto(e){
     }else{
       existe.agregarCantidad(1);
     }
+    /* Se guarda en el storage */
     localStorage.setItem('carrito',JSON.stringify(carrito));
+    /* Se muestra en el carrito */
     carritoHTML(carrito);
   }
 
-
 /* Función para mostrar carrito en el HTML  */
 
-function carritoHTML(ProductosSkincare){
-    $('#carritoCantidad').html(ProductosSkincare.length);
-    $('#carritoProductos').empty();
-    for (const producto of ProductosSkincare) {
-      $('#carritoProductos').append(`<p> ${producto.producto} 
-                                      <span class="badge badge-warning">
-                                      $ ${producto.precio}</span>
-                                      <span class="badge badge-warning">
-                                      Cantidad: ${producto.cantidad}</span>
-                                      <span class="badge badge-warning">
-                                      Subtotal: ${producto.subTotal()}</span>                                     
-                                      </p>`);
-    }
-  }
+function carritoHTML(productos){
+  $('#carritoCantidad').html(productos.length);
+  $('#carritoProductos').empty();
+  for (const producto of productos) {
+    $("#carritoProductos").append(`
+    <p> ${producto.producto} 
+    <span class="badge badge-warning"> $ ${producto.precio}</span>
+    <span class="badge badge-warning"> Cantidad: ${producto.cantidad}</span>
+    <span class="badge badge-warning"> Subtotal: ${producto.subTotal()}</span>                                     
+    </p>`);
+}
+}
+    
+  
+ 
+
   
   
 
-/* FUNCIONES BACK OFFICE - COMENTADAS POR EL MOMENTO */
+/* FUNCIONES BACK OFFICE - COMENTADAS POR EL MOMENTO
 
- /* Busqueda de productos por su ID */
+  Busqueda de productos por su ID 
 let busquedaID = document.getElementById("insertarIDproducto")
 let IDingresada = busquedaID.children
 let IDencontrada = ProductosSkincare.find( producto => producto.id == IDingresada)
@@ -66,7 +84,7 @@ busquedaID.onsubmit = (e) => {
     console.log(IDencontrada)
 }
 
-/*  Función para mostrar ID encontrada en DOM */
+  Función para mostrar ID encontrada en DOM 
 
 function resultadoIDenDOM() {
     productoIDIngresado.innerHTML="";
@@ -74,5 +92,5 @@ function resultadoIDenDOM() {
         divID.innerHTML= "El producto encontrado es " + JSON.stringify(IDencontrada);
         productoIDIngresado.appendChild(divID);
     }
-
+    */
   
--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
 
+/* INFORMACIÓN DE PRODUCTOS */
+
+/* Array tipos */
+
+tiposProductos = ["limpiador", "serum", "bloqueador"]
+
+
 /* Añadiendo los objetos al array global  */
 
-ProductosSkincare.push(new Productos (1, "Limpiador en Gel", 50, "limpiador", "https://i.ibb.co/vmL6zGK/pexels-photo-5797994.jpg"));
-ProductosSkincare.push(new Productos (2, "Limpiador Espuma", 30, "limpiador", "https://i.ibb.co/xS9mh79/pexels-photo-4735905.jpg"))
-ProductosSkincare.push(new Productos (3, "leche limpiadora", 40, "limpiador", "https://i.ibb.co/rxKHVwp/pexels-photo-7319145.jpg"))
-ProductosSkincare.push(new Productos (4,"serum vitamina C", 70, "serum", "https://i.ibb.co/FJJSQpm/pexels-photo-8131586.jpg"))
-ProductosSkincare.push(new Productos (5,"serum hidratante", 80, "serum", "https://i.ibb.co/8N2YPf2/pexels-photo-5069440.jpg"))
-ProductosSkincare.push(new Productos (6,"bloqueador gel", 60, "bloqueador", "https://i.ibb.co/vzVv0BF/pexels-photo-8155447.jpg"))
-console.log(ProductosSkincare)
+ProductosSkincare.push(new Productos (1, "Limpiador en Gel", 50, tiposProductos[0], "https://i.ibb.co/vmL6zGK/pexels-photo-5797994.jpg"));
+ProductosSkincare.push(new Productos (2, "Limpiador Espuma", 30, tiposProductos[0], "https://i.ibb.co/xS9mh79/pexels-photo-4735905.jpg"))
+ProductosSkincare.push(new Productos (3, "leche limpiadora", 40, tiposProductos[0], "https://i.ibb.co/rxKHVwp/pexels-photo-7319145.jpg"))
+ProductosSkincare.push(new Productos (4,"serum vitamina C", 70, tiposProductos[1], "https://i.ibb.co/FJJSQpm/pexels-photo-8131586.jpg"))
+ProductosSkincare.push(new Productos (5,"serum hidratante", 80, tiposProductos[1], "https://i.ibb.co/8N2YPf2/pexels-photo-5069440.jpg"))
+ProductosSkincare.push(new Productos (6,"bloqueador gel", 60, tiposProductos[2], "https://i.ibb.co/vzVv0BF/pexels-photo-8155447.jpg"))
+
+
+
+
 
 /* Añadiendo estos productos al HTML */
 productosHTML(ProductosSkincare, '#productosContenedor');
 
 
-/* Verificar que se cargo el dom */
-$(document).ready(function () {
-    console.log('Se cargo el Dom');
-
-
-
-/* Agregando evento para el boton de Agregar a  carrito */
+/* Funcionalidad de comprar */
 $('.btn-agregar').click(comprarProducto);
+
+
 
 /* Almacenando datos del carrito */
 
@@ -34,10 +40,10 @@ if ("carrito" in localStorage) {
 
 console.log(carrito);
 
-/* Mostrando resultados en carrito */
+/* Mostrar información del carrito*/
 
-carritoHTML(carrito);
+carritoHTML(carrito); 
 
-});
+
 
 
